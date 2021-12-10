a46a5b7183815a4873d081b37f1a57fc9ce7439a
Nicholas Birkbeck <nbirkbeck@gmail.com>
2021-12-10 13:31:40 +1100

    Turned loadout selection into form, preparing to connect state

--- src/components/LoadoutSelection.js
+++ src/components/LoadoutSelection.js
@@ -18,41 +18,51 @@ import {
 } from "./weaponOptions";
 
 function LoadoutSelection() {
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log("Selected Helmet: " + e.target.helmet.value);
+    console.log("Selected Earpiece: " + e.target.earpiece.value);
+  };
+
   return (
     <>
-      <ItemSelect id="helmet" label="Helmet" options={headwearOptions} />
-      <ItemSelect id="earpiece" label="Earpiece" options={earpieceOptions} />
-      <ItemSelect
-        id="faceCover"
-        label="Face Cover"
-        options={faceCoverOptions}
-      />
-      <ItemSelect id="armband" label="Armband" options={armbandOptions} />
-      <ItemSelect
-        id="bodyarmor"
-        label="Body Armor"
-        options={bodyArmorOptions}
-      />
-      <ItemSelect id="eyewear" label="Eyewear" options={eyewearOptions} />
-      <ItemSelect
-        id="tacticalRig"
-        label="Tactical Rig"
-        options={tacticalRigOptions}
-      />
-      <ItemSelect id="backpack" label="Backpack" options={backpackOptions} />
-      <br />
-      <ItemSelect
-        id="primaryWeapon"
-        label="Primary Weapon"
-        options={primaryWeaponOptions}
-      />
-      <ItemSelect
-        id="secondaryWeapon"
-        label="Secondary Weapon"
-        options={primaryWeaponOptions}
-      />
-      <ItemSelect id="sidearm" label="Sidearm" options={holsterOptions} />
-      <ItemSelect id="scabbard" label="Scabbard" options={scabbardOptions} />
+      <form onSubmit={onSubmit}>
+        <ItemSelect id="helmet" label="Helmet" options={headwearOptions} />
+        <ItemSelect id="earpiece" label="Earpiece" options={earpieceOptions} />
+        <ItemSelect
+          id="faceCover"
+          label="Face Cover"
+          options={faceCoverOptions}
+        />
+        <ItemSelect id="armband" label="Armband" options={armbandOptions} />
+        <ItemSelect
+          id="bodyarmor"
+          label="Body Armor"
+          options={bodyArmorOptions}
+        />
+        <ItemSelect id="eyewear" label="Eyewear" options={eyewearOptions} />
+        <ItemSelect
+          id="tacticalRig"
+          label="Tactical Rig"
+          options={tacticalRigOptions}
+        />
+        <ItemSelect id="backpack" label="Backpack" options={backpackOptions} />
+        <br />
+        <ItemSelect
+          id="primaryWeapon"
+          label="Primary Weapon"
+          options={primaryWeaponOptions}
+        />
+        <ItemSelect
+          id="secondaryWeapon"
+          label="Secondary Weapon"
+          options={primaryWeaponOptions}
+        />
+        <ItemSelect id="sidearm" label="Sidearm" options={holsterOptions} />
+        <ItemSelect id="scabbard" label="Scabbard" options={scabbardOptions} />
+
+        <button type="submit">Save</button>
+      </form>
     </>
   );
 }
